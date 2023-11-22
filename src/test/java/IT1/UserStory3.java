704117b967e95897f36538e3c48b75aae398995d
cristianFleita <cristianagustinfleita@gmail.com>
2023-11-21 21:59:17 -0300

    Updated US3 test

--- src/test/java/IT1/UserStory3.java
+++ src/test/java/IT1/UserStory3.java
@@ -1,16 +1,14 @@
 package IT1;
 
 import core.ChefExpress;
-import core.RecipesUpdater;
-import entities.Recipe;
-import finders.LocalRecipesFactory;
+import core.HistoricalRecipesCounter;
 import core.RecipesProvider;
+import entities.Recipe;
 import interfaces.RecipeScorer;
-import interfaces.RecipesFactory;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import core.HistoricalRecipesCounter;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,7 +20,6 @@ public class UserStory3
 {
     private HistoricalRecipesCounter historicalRecipesCounter;
     private RecipeScorer scorerMock;
-
     private RecipesProvider recipesProvider;
     private ChefExpress chefExpress;
     private List<Recipe> recipes;
@@ -30,13 +27,27 @@ public class UserStory3
     @BeforeEach
     public void setUp()
     {
-        List<Integer> recipeIds = Arrays.asList(1, 2);
-        recipes = mockRecipes(recipeIds);
+        List<Integer> recipeIds = Arrays.asList(1, 2, 3);
+        this.recipes = mockRecipes(recipeIds);
 
-        scorerMock = mock(RecipeScorer.class);
-        recipesProvider = this.createRecipesProvider(recipes);
-        chefExpress = new ChefExpress(recipesProvider, scorerMock, new HashMap<>());
-        historicalRecipesCounter = new HistoricalRecipesCounter(chefExpress);
+        Map<Recipe, Integer> scorerResults = Map.of(recipes.get(0),0,
+                                                    recipes.get(1), 10,
+                                                    recipes.get(2), 10);
+
+        this.scorerMock = mock(RecipeScorer.class);
+        this.setScorerResult(scorerResults);
+
+        this.recipesProvider = mock(RecipesProvider.class);
+
+        this.chefExpress = new ChefExpress(recipesProvider, scorerMock, new HashMap<>());
+        this.historicalRecipesCounter = new HistoricalRecipesCounter(chefExpress);
+    }
+
+    private List<Recipe> mockRecipes(List<Integer> ids)
+    {
+        return ids.stream()
+                .map(recipeId -> new Recipe(recipeId, "R" + recipeId, new HashMap<>()))
+                .collect(Collectors.toList());
     }
 
     private void setScorerResult(Map<Recipe, Integer> scorerResult)
@@ -51,55 +62,85 @@ public class UserStory3
     @Description("Sin recetas recomendadas")
     public void ca1SinRecetasRecomendadas()
     {
-        Map<Recipe, Integer> scorerResults = Map.of(recipes.get(0),0, recipes.get(1), 0);
-        this.setScorerResult(scorerResults);
+        // recipes.get(0) == R1
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(0)));
         this.chefExpress.recommend();
+
         assertTrue(historicalRecipesCounter.getHistoricRecipes().isEmpty());
     }
 
     @Test
-    @Description("Recetas recomendadas múltiples veces")
-    public void ca2RecetasRecomendadasMultiplesVeces()
+    @Description("Receta recomendada una unica vez")
+    public void ca2RecetaRecomendadaUnaUnicaVez()
     {
-
-        this.mockScorerResults(10, 10);
-        this.chefExpress.recommend();
-        this.mockScorerResults(10, 0);
+        // recipes.get(1) == R2
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(1)));
         this.chefExpress.recommend();
 
-        assert historicalRecipesCounter.getHistoricRecipes().containsKey(recipes.get(0)) && historicalRecipesCounter.getHistoricRecipes().containsKey(recipes.get(1));
-        assert historicalRecipesCounter.getHistoricRecipes().get(recipes.get(0)) == 2  && historicalRecipesCounter.getHistoricRecipes().get(recipes.get(1)) == 1;
+        Map<Recipe, Integer> historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert historicalRecipesCounter.getHistoricRecipes().get(recipes.get(1)) == 1;
     }
 
     @Test
-    @Description("Receta recomendada una única vez")
-    public void ca3RecetaRecomendadaUnaUnicaVez()
+    @Description("Multiples recetas recomendadas")
+    public void ca3MultiplesRecetasRecomendadas()
     {
-        this.mockScorerResults(10, 0);
+        // recipes.get(1) == R2 y recipes.get(2) == R3
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(1), this.recipes.get(2)));
         this.chefExpress.recommend();
-        assert historicalRecipesCounter.getHistoricRecipes().containsKey(recipes.get(0));
-        assert historicalRecipesCounter.getHistoricRecipes().get(recipes.get(0)) == 1;
+
+        Map<Recipe, Integer> historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert historicRecipesResult.containsKey(this.recipes.get(2));
+
+        assert  historicRecipesResult.get(recipes.get(1)) == 1;
+        assert  historicRecipesResult.get(recipes.get(2)) == 1;
     }
 
-
-    private List<Recipe> mockRecipes(List<Integer> ids)
+    @Test
+    @Description("Receta recomendada multiples veces")
+    public void ca4RecetaRecomendadaMultiplesVeces()
     {
-        return ids.stream()
-                .map(recipeId -> new Recipe(recipeId, "recipe " + recipeId, new HashMap<>()))
-                .collect(Collectors.toList());
+        // recipes.get(1) == R2
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(1)));
+        this.chefExpress.recommend();
+
+        Map<Recipe, Integer> historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert  historicRecipesResult.get(recipes.get(1)) == 1;
+
+        this.chefExpress.recommend();
+
+        historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert  historicRecipesResult.get(recipes.get(1)) == 2;
     }
 
-    private void mockScorerResults(Integer scoreRecipe1, Integer scoreRecipe2) {
-        Map<Recipe, Integer> scorerResults = Map.of(recipes.get(0),scoreRecipe1, recipes.get(1), scoreRecipe2);
-        this.setScorerResult(scorerResults);
-    }
+    @Test
+    @Description("Sin recetas recomendadas al historico con contenido")
+    public void ca5SinRecetasRecomendadasAlHistoricoConContenido()
+    {
+        // recipes.get(1) == R2
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(1)));
+        this.chefExpress.recommend();
 
-    private RecipesProvider createRecipesProvider(List<Recipe> recipesList) {
-        RecipesFactory recipesLocalFinder = new LocalRecipesFactory();
-        String recipesPath =  "";
-        HashSet<Recipe> recipes = new HashSet<>(recipesList);
-        RecipesUpdater recipesUpdater = new RecipesUpdater(recipesLocalFinder,  List.of(recipesPath.split(",")),  recipes);
-        RecipesProvider recipesProvider = new RecipesProvider(recipesUpdater);
-        return recipesProvider;
+        Map<Recipe, Integer> historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert  historicRecipesResult.get(recipes.get(1)) == 1;
+
+        // recipes.get(0) == R1
+        when(recipesProvider.getRecipes()).thenReturn(Set.of(this.recipes.get(0)));
+        this.chefExpress.recommend();
+
+        historicRecipesResult = historicalRecipesCounter.getHistoricRecipes();
+
+        assert historicRecipesResult.containsKey(this.recipes.get(1));
+        assert  historicRecipesResult.get(recipes.get(1)) == 1;
     }
 }
